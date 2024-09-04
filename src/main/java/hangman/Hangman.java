af87fc0c5202bb6ade283034be6a92a3a42ad709
saulelabra <quiquelabra@gmail.com>
2024-09-04 17:18:25 -0600

    Hangman game finished

--- src/main/java/hangman/Hangman.java
+++ src/main/java/hangman/Hangman.java
@@ -1,20 +1,26 @@
 package hangman;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
-public class Hangman {
+public class Hangman{
     final private String secretWord;
     private HashMap<Character, ArrayList<Integer>> secretWordMap;
+    private int maxAttempts;
     private int attempts;
     private StringBuilder guessedWord;
     private HashSet<Character> guessedLetters;
+    private StringBuilder hangmanDrawing;
 
-    Hangman(String secretWord, int maxAttempts){
+    Hangman(String secretWord, int maxAttempts) throws IOException{
         this.secretWord = secretWord.toLowerCase();
+        this.maxAttempts = maxAttempts;
         this.attempts = maxAttempts;
         guessedWord = new StringBuilder();
         guessedWord.append("_".repeat(secretWord.length()));
         guessedLetters = new HashSet<>();
+        hangmanDrawing = new StringBuilder();
 
         //Initialize secretWordMap to store indexes of chars
         secretWordMap = new HashMap<>();
@@ -25,6 +31,46 @@ public class Hangman {
                 secretWordMap.put(this.secretWord.charAt(i), new ArrayList<>(Arrays.asList(i)));
             }
         }
+
+        //Initialize String for hangman drawing from txt file
+        try (FileReader reader = new FileReader("./src/main/java/hangman/hangmanPicture.txt")){
+            int data ;
+            while((data = reader.read()) != -1){
+                hangmanDrawing.append((char)data);
+            }
+        }catch (IOException e){
+            throw(e);
+        }
+    }
+
+    public void printHangman(){
+        int totalChars = 30;
+        int totalLineBreaks = 5;
+        int lineBreakCounter = 0;
+
+        //Get number of parts to print in each iteration (floored)
+        int partsPerAttempt = (int)Math.floor((double) totalChars /maxAttempts);
+        int attemptsUsed = maxAttempts-attempts;
+        int partsToPrint;
+
+        //If division is not exact sum the difference
+        if(totalChars%maxAttempts != 0){
+            partsToPrint = partsPerAttempt*attemptsUsed+(totalChars%maxAttempts);
+        }else{
+            partsToPrint = partsPerAttempt*attemptsUsed;
+        }
+
+        //Print hangman
+        for(int i=0; i<partsToPrint; i++){
+            if(hangmanDrawing.charAt(i) == '\n'){
+                lineBreakCounter++;
+            }
+
+            System.out.print(hangmanDrawing.toString().charAt(i));
+        }
+
+        //Print placeholder line breaks
+        for(int i=0; i<totalLineBreaks-lineBreakCounter+1; i++) System.out.print('\n');
     }
 
     public void play(){
@@ -34,6 +80,13 @@ public class Hangman {
         System.out.println("********************Welcome to Hangman********************");
 
         while(attempts > 0){
+            System.out.println("**********************************************************");
+            //Print attempts remaining
+            System.out.println("Attempts: " + attempts);
+
+            //Print hangman
+            printHangman();
+
             //Show game status (blank spaces and already guessed letters)
             System.out.println(guessedWord.toString());
 
@@ -45,8 +98,6 @@ public class Hangman {
                 //If letter already guessed prompt again
                 if(guessedLetters.contains(currentGuess)) System.out.println("Letter already attempted, try again!");
             } while (guessedLetters.contains(currentGuess));
-
-            //Register guess in game status-----------
 
             //Register guess in guessedLetters
             guessedLetters.add(currentGuess);
@@ -63,12 +114,25 @@ public class Hangman {
             }
 
             //If word is guessed completely return
-            
-            //System.out.println("You won!");
-            //return;
+            if(!guessedWord.toString().contains("_")) {
+                System.out.println("You won!");
+                return;
+            }
+            System.out.println("**********************************************************");
         }
 
         //End game if attempts are finished
+
+        System.out.println("**********************************************************");
+        //Print attempts remaining
+        System.out.println("Attempts: " + attempts);
+
+        //Print hangman
+        printHangman();
+
+        //Show game status (blank spaces and already guessed letters)
+        System.out.println(guessedWord.toString());
         System.out.println("You ran out of attempts. Better luck next time!");
+        System.out.println("**********************************************************");
     }
 }
--- src/main/java/hangman/HangmanGame.java
+++ src/main/java/hangman/HangmanGame.java
@@ -1,8 +1,8 @@
 package hangman;
 
 public class HangmanGame {
-    public static void main(String[] args) {
-        Hangman game = new Hangman("Secret", 3);
+    public static void main(String[] args) throws Exception {
+        Hangman game = new Hangman("Secret", 8);
         game.play();
     }
 }
